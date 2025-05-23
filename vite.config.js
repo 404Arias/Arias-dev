75695ce1ffdc50e91251454a8f5fc708c8d90e15
404arias <207381286+404Arias@users.noreply.github.com>
2025-05-23 18:35:04 -0300

    Update main.jsx and vite configuration files

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://github.com/404Arias/AriasDev.github.io",
+  base: "https://404Arias.github.io/AriasDev.github.io",
 });
